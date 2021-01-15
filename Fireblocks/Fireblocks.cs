25c3d87ea7b87b43f18321b09c28dbacb3c49486
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-15 15:18:40 -0300

    add get vault and create vault methods

--- Fireblocks/Fireblocks.cs
+++ Fireblocks/Fireblocks.cs
@@ -5,22 +5,21 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using Fireblocks.Entities;
+using Fireblocks.Services;
+using System.Collections.Generic;
 
 namespace Fireblocks
 {
     public class Fireblocks : IFireblocks
     {
         private const string _messageErrorHttpClient = "There was an error in HttpClient";
+        private const string _messageErrorInvalidInputParameters = "Invalid input parameters";
 
-        private readonly string _apiKey;
-        private readonly string _privateKey;
-        private readonly HttpClient _httpClient;
+        private readonly IFireblocksClient _fireblocksClient;
 
-        public Fireblocks(string apiKey, string privateKey, HttpClient httpClient)
+        public Fireblocks(IFireblocksClient fireblocksClient)
         {
-            _apiKey = apiKey;
-            _privateKey = privateKey;
-            _httpClient = httpClient;
+            _fireblocksClient = fireblocksClient;
         }
 
         /// <summary>
@@ -29,14 +28,37 @@ namespace Fireblocks
         /// <returns>
         /// Returns an array of VaultAccount objects.
         /// </returns>
-        public async Task<VaultAccount> GetVault()
+        public async Task<List<VaultAccount>> GetVaults()
         {
             try
             {
-                string requestUri = "vault/accounts";
+                string requestUri = "/v1/vault/accounts";
 
-                SetAuthenticationHeader(requestUri);
-                VaultAccount vaultAccount = await _httpClient.GetFromJsonAsync<VaultAccount>(requestUri);
+                List<VaultAccount> vaultAccounts = await _fireblocksClient.GetAsync<List<VaultAccount>>(requestUri);
+                return vaultAccounts;
+            }
+            catch (Exception ex)
+            {
+                throw new FireblocksException(_messageErrorHttpClient, ex);
+            }
+        }
+
+        /// <summary>
+        /// Returns the requested Vault Account.
+        /// </summary>
+        /// <param name="vaultAccountId"></param>
+        /// <returns></returns>
+        public async Task<VaultAccount> GetVault(string vaultAccountId)
+        {
+            if (string.IsNullOrEmpty(vaultAccountId))
+            {
+                throw new ArgumentException(_messageErrorInvalidInputParameters);
+            }
+            try
+            {
+                string requestUri = $"/v1/vault/accounts/{vaultAccountId}";
+
+                VaultAccount vaultAccount = await _fireblocksClient.GetAsync<VaultAccount>(requestUri);
                 return vaultAccount;
             }
             catch (Exception ex)
@@ -45,16 +67,38 @@ namespace Fireblocks
             }
         }
 
-        private void SetAuthenticationHeader(string path)
+        /// <summary>
+        /// Creates a new Vault Account with the requested name.
+        /// </summary>
+        /// <param name="name">The name of the new account (this can be renamed later)</param>
+        /// <param name="hiddenOnUI">[optional] Should be set to true if you wish this account will not appear in the web console, false by default</param>
+        /// <param name="customerRefId">[optional] The ID for AML providers to associate the owner of funds with transactions</param>
+        /// <param name="autoFuel">[optional] In case the Gas Station service is enabled on your workspace, this flag needs to be set to "true" if you wish to add this account's Ethereum address to be monitored and fueled upon detected deposits of ERC20 tokens.</param>
+        /// <returns></returns>
+        public async Task<VaultAccount> CreateVault(string name, bool? hiddenOnUI = null, string customerRefId = null, bool? autoFuel = null)
         {
-            //string jwt = OAuthToken.CreateSignedToken(_apiKey, path, _privateKey);
-            //bool removed = _httpClient.DefaultRequestHeaders.Remove("Bearer");
-            //if (!removed)
-            //{
-            //    throw new Exception("Header not found");
-            //}
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(_messageErrorInvalidInputParameters);
+            }
+            try
+            {
+                CreateVaultAccount createVaultAccount = new CreateVaultAccount
+                {
+                    Name = name,
+                    HiddenOnUI = hiddenOnUI,
+                    CustomerRefId = customerRefId,
+                    AutoFuel = autoFuel
+                };
+                string requestUri = "/v1/vault/accounts";
 
-            //_httpClient.DefaultRequestHeaders.Add("Bearer", jwt);
+                VaultAccount vaultAccount = await _fireblocksClient.PostAsync<VaultAccount, CreateVaultAccount>(requestUri, createVaultAccount);
+                return vaultAccount;
+            }
+            catch (Exception ex)
+            {
+                throw new FireblocksException(_messageErrorHttpClient, ex);
+            }
         }
     }
 }
