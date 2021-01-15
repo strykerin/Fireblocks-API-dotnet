7b911493117439c0571bbfcd15ae43a714cd9358
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-15 16:44:30 -0300

    add constrains on type parameters

--- Fireblocks/Services/IFireblocksClient.cs
+++ Fireblocks/Services/IFireblocksClient.cs
@@ -4,7 +4,9 @@ namespace Fireblocks.Services
 {
     public interface IFireblocksClient
     {
-        Task<T> GetAsync<T>(string requestUri);
-        Task<T> PostAsync<T, TBody>(string requestUri, TBody requestBody);
+        Task<T> GetAsync<T>(string requestUri) where T : class;
+        Task GetAsync(string requestUri);
+        Task<T> PostAsync<T, TBody>(string requestUri, TBody requestBody) where T : class 
+                                                                          where TBody : class;
     }
 }
