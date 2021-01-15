2df0b9b231c82c5aab3cbaaf91c8ffb6c2b940f4
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-15 15:59:05 -0300

    add new methods to fireblocks interface

---
+++ Fireblocks/Services/IFireblocksClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Fireblocks.Services
+{
+    public interface IFireblocksClient
+    {
+        Task<T> GetAsync<T>(string requestUri);
+        Task<T> PostAsync<T, TBody>(string requestUri, TBody requestBody);
+    }
+}
