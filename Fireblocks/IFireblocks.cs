c2a928652e7be3aac907ab4cbad44bd3378b19f2
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-15 16:43:41 -0300

    add methos to firebloks interface

--- Fireblocks/IFireblocks.cs
+++ Fireblocks/IFireblocks.cs
@@ -7,5 +7,10 @@ namespace Fireblocks
     public interface IFireblocks
     {
         Task<List<VaultAccount>> GetVaults();
+        Task<VaultAccount> GetVault(string vaultAccountId);
+        Task<VaultAccount> CreateVault(string name, bool? hiddenOnUI = null, string customerRefId = null, bool? autoFuel = null);
+        Task<VaultAsset> GetVaultWallet(string vaultAccountId, string assetId);
+        Task<CreateVaultAssetResponse> CreateNewWalletForVault(string vaultAccountId, string assetId, string EosAccountName = null);
+        Task HideVaultFromWebConsoleView(string vaultAccountId);
     }
 }
