76d6e3c47ecbd99895c6421f4d168d615d464902
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-15 15:40:20 -0300

    create vault account class

---
+++ Fireblocks/Entities/CreateVaultAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fireblocks.Entities
+{
+    public class CreateVaultAccount
+    {
+        public string Name { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? HiddenOnUI { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string CustomerRefId { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? AutoFuel { get; set; }
+    }
+}
