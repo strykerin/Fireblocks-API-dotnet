0aa13734d5a0fb99560ce57c9605656b49675cb7
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-18 16:09:14 -0300

    create new methods

--- Fireblocks/Fireblocks.cs
+++ Fireblocks/Fireblocks.cs
@@ -98,6 +98,12 @@ namespace Fireblocks
             }
         }
 
+        /// <summary>
+        /// Retrieves a wallet of a specific asset under a Fireblocks Vault Account.
+        /// </summary>
+        /// <param name="vaultAccountId">The ID of the vault account to return, or 'default' for the default vault account</param>
+        /// <param name="assetId">The ID of the asset</param>
+        /// <returns></returns>
         public async Task<VaultAsset> GetVaultWallet(string vaultAccountId, string assetId)
         {
             if (string.IsNullOrEmpty(vaultAccountId) || string.IsNullOrEmpty(assetId))
@@ -117,6 +123,13 @@ namespace Fireblocks
             }
         }
 
+        /// <summary>
+        /// Creates a new wallet of a specific asset under a Fireblocks Vault Account.
+        /// </summary>
+        /// <param name="vaultAccountId">The ID of the vault account, or 'default' for the default vault account</param>
+        /// <param name="assetId">The ID of the asset</param>
+        /// <param name="EosAccountName">[optional] EOS account address</param>
+        /// <returns></returns>
         public async Task<CreateVaultAssetResponse> CreateNewWalletForVault(string vaultAccountId, string assetId, string EosAccountName = null)
         {
             if (string.IsNullOrEmpty(vaultAccountId) || string.IsNullOrEmpty(assetId))
@@ -141,6 +154,11 @@ namespace Fireblocks
             }
         }
 
+        /// <summary>
+        /// Hides the Vault Account from the web console view.
+        /// </summary>
+        /// <param name="vaultAccountId">The ID of the vault account to hide from the web console</param>
+        /// <returns></returns>
         public async Task HideVaultFromWebConsoleView(string vaultAccountId)
         {
             if (string.IsNullOrEmpty(vaultAccountId))
@@ -151,6 +169,86 @@ namespace Fireblocks
             {
                 string requestUri = $"/v1/vault/accounts/{vaultAccountId}/hide";
                 await _fireblocksClient.GetAsync(requestUri);
+            }
+            catch (Exception ex)
+            {
+                throw new FireblocksException(_messageErrorHttpClient, ex);
+            }
+        }
+
+        /// <summary>
+        /// Returns the Vault Account to be visible in the web console.
+        /// </summary>
+        /// <param name="vaultAccountId">The ID of the vault account to be visible in the web console</param>
+        /// <returns></returns>
+        public async Task UnhideVaultFromWebConsoleView(string vaultAccountId)
+        {
+            if (string.IsNullOrEmpty(vaultAccountId))
+            {
+                throw new FireblocksException(_messageErrorInvalidInputParameters);
+            }
+            try
+            {
+                string requestUri = $"/v1/vault/accounts/{vaultAccountId}/unhide";
+                await _fireblocksClient.GetAsync(requestUri);
+            }
+            catch (Exception ex)
+            {
+                throw new FireblocksException(_messageErrorHttpClient, ex);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all addresses of a specific asset inside a Fireblocks Vault Account.
+        /// </summary>
+        /// <param name="vaultAccountId">The ID of the vault account to return, or 'default' for the default vault account</param>
+        /// <param name="assetId">The ID of the asset</param>
+        /// <param name="EosAccountName">[optional] EOS account address</param>
+        /// <returns></returns>
+        public async Task<List<VaultWalletAddress>> GetAddressesOfVaultWallet(string vaultAccountId, string assetId, string eosAccountName = null)
+        {
+            if (string.IsNullOrEmpty(vaultAccountId) || string.IsNullOrEmpty(assetId))
+            {
+                throw new FireblocksException(_messageErrorInvalidInputParameters);
+            }
+            try
+            {
+                string requestUri = $"/v1/vault/accounts/{vaultAccountId}/{assetId}/addresses";
+                CreateWalletForVault requestBody = new CreateWalletForVault
+                {
+                    EosAccountName = eosAccountName
+                };
+                List<VaultWalletAddress> vaultWalletAddress = await _fireblocksClient.PostAsync<List<VaultWalletAddress>, CreateWalletForVault>(requestUri, requestBody);
+                return vaultWalletAddress;
+            }
+            catch (Exception ex)
+            {
+                throw new FireblocksException(_messageErrorHttpClient, ex);
+            }
+        }
+
+        /// <summary>
+        /// Lists the transaction history for your workspace.
+        /// </summary>
+        /// <param name="before">[optional] Unix timestamp in milliseconds. Returns only transactions created before the specified date</param>
+        /// <param name="after">[optional] Unix timestamp in milliseconds. Returns only transactions created after the specified date</param>
+        /// <param name="status">[optional] Comma-separated list of statuses. Returns only transactions with each of the specified statuses</param>
+        /// <param name="orderBy">[optional] The field to order the results by. Available values : createdAt (default), lastUpdated</param>
+        /// <param name="sourceType">[optional] The source type of the transaction. Available values: VAULT_ACCOUNT, EXCHANGE_ACCOUNT, INTERNAL_WALLET, EXTERNAL_WALLET, FIAT_ACCOUNT, NETWORK_CONNECTION, COMPOUND</param>
+        /// <param name="sourceId">[optional] The source ID of the transaction</param>
+        /// <param name="destType">[optional] The destination type of the transaction. Available values: VAULT_ACCOUNT, EXCHANGE_ACCOUNT, INTERNAL_WALLET, EXTERNAL_WALLET, FIAT_ACCOUNT, NETWORK_CONNECTION, COMPOUND</param>
+        /// <param name="destId">[optional] The destination ID of the transaction</param>
+        /// <param name="assets">[optional] A list of assets to filter by, seperated by commas</param>
+        /// <param name="txHash">[optional] Returns only results with a specified txHash</param>
+        /// <param name="limit">[optional] Limits the number of returned transactions. If not provided, a defult of 200 will be returned. The maximum allowed limit is 500.</param>
+        /// <returns></returns>
+        public async Task<List<TransactionDetails>> ListTransactions(string before = null, string after = null, string status = null, string orderBy = null, string sourceType = null, string sourceId = null, string destType = null, string destId = null, string assets = null, string txHash = null, string limit = null)
+        {
+            try
+            {
+                string requestUri = $"/v1/transactions";
+                List<TransactionDetails> transactionsDetails = await _fireblocksClient.GetAsync<List<TransactionDetails>>(requestUri);
+                return transactionsDetails;
             }
             catch (Exception ex)
             {
