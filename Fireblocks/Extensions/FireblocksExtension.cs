e785f655975e52d6473a4b95b60c57f8d548138e
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-18 16:20:36 -0300

    add private key to be passed in extension method

--- Fireblocks/Extensions/FireblocksExtension.cs
+++ Fireblocks/Extensions/FireblocksExtension.cs
@@ -13,8 +13,12 @@ namespace Fireblocks.Extensions
         /// </summary>
         /// <param name="services">IServiceCollection</param>
         /// <param name="apiKey">API Key to access Fireblocks</param>
-        public static void AddFireblocks(this IServiceCollection services, string apiKey)
+        public static void AddFireblocks(this IServiceCollection services, string apiKey, string privateKey)
         {
+            if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(privateKey))
+            {
+                throw new ArgumentNullException("Input parameters are null or empty");
+            }
             services.AddHttpClient("Fireblocks", httpClient =>
             {
                 httpClient.BaseAddress = new Uri(FireblocksParameters.BaseAddress);
@@ -26,7 +30,7 @@ namespace Fireblocks.Extensions
                 var clientFactory = ctx.GetRequiredService<IHttpClientFactory>();
                 var httpClient = clientFactory.CreateClient("Fireblocks");
 
-                return new FireblocksClient(httpClient, apiKey);
+                return new FireblocksClient(httpClient, apiKey, privateKey);
             });
 
             services.AddScoped<IFireblocks>(ctx =>
