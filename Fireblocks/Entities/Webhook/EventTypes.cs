eb04ea56e0c9394802fe5d42d742689685928d43
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-19 20:19:21 -0300

    create eventtypes enum

---
+++ Fireblocks/Entities/Webhook/EventTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace Fireblocks.Entities.Webhook
+{
+    public enum EventTypes
+    {
+        TRANSACTION_CREATED = 0,
+        TRANSACTION_STATUS_UPDATED = 1,
+        VAULT_ACCOUNT_ADDED = 2,
+        VAULT_ACCOUNT_ASSET_ADDED = 3,
+        INTERNAL_WALLET_ASSET_ADDED = 4,
+        EXTERNAL_WALLET_ASSET_ADDED = 5,
+        EXCHANGE_ACCOUNT_ADDED = 6,
+        FIAT_ACCOUNT_ADDED = 7,
+        NETWORK_CONNECTION_ADDED = 8
+    }
+}
