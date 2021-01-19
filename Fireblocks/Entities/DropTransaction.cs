9806b309488cee6e7cbfa51ba34433cfeafd58e8
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-19 15:34:44 -0300

    create DropTransaction class

---
+++ Fireblocks/Entities/DropTransaction.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fireblocks.Entities
+{
+    public class DropTransaction
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string FeeLevel { get; set; }
+    }
+}
