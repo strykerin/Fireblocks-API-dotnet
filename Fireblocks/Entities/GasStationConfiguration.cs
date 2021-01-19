52629b6ebf89fbab94b20db1fbffbabdff3d6e24
strykerin <dacosta.pereirafabio@gmail.com>
2021-01-19 17:38:02 -0300

    create GasStation classes

---
+++ Fireblocks/Entities/GasStationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Fireblocks.Entities
+{
+    public class GasStationConfiguration
+    {
+        /// <summary>
+        /// Below this ETH balane the address will be funded up until gasCap value, in ETH units
+        /// </summary>
+        public string GasThreshold { get; set; }
+
+        /// <summary>
+        /// Up to this level the address will be funded with ETH, in ETH units
+        /// </summary>
+        public string GasCap { get; set; }
+
+        /// <summary>
+        /// The funding transaction will be sent with this maximum value gas price, in Gwei units
+        /// </summary>
+        public string MaxGasPrice { get; set; }
+    }
+}
---
+++ Fireblocks/Entities/GasStationInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Fireblocks.Entities
+{
+    public class GasStationInfo
+    {
+        /// <summary>
+        /// A dictionary of an asset and its balance in the Gas Station
+        /// </summary>
+        public Dictionary<string, string> Balance { get; set; }
+
+        /// <summary>
+        /// The settings of your Gas Station
+        /// </summary>
+        public GasStationConfiguration Configuration { get; set; }
+    }
+}
